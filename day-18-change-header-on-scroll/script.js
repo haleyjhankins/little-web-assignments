464678bc0ab17ceedc2de4e0dc01758d0b71ab5a
haleyjhankins <haley.j.hankins@gmail.com>
2017-04-05 18:49:27 -0500

    Changed header colors, added images and fonts.

--- day-18-change-header-on-scroll/script.js
+++ day-18-change-header-on-scroll/script.js
@@ -2,11 +2,15 @@ var nav= document.querySelector('nav');
 // var isHidden= false;
 
 window.addEventListener('scroll', function(){
-  if (window.scrollY < 200){
-    nav.style.backgroundColor= 'red';
+  if (window.scrollY < 100){
+    nav.style.backgroundColor= '#BDBFBD';
   }
 
-  else if(window.scrollY >= 200){
-    nav.style.backgroundColor= 'blue';
+  else if(window.scrollY >= 200 && window.scrollY < 300){
+    nav.style.backgroundColor= '#3C4526';
+  }
+
+  else if(window.scrollY >= 300 ){
+    nav.style.backgroundColor= '#AF9C84';
   }
 });
