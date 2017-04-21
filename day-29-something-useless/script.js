bb6cf08b55837e007d516d0f23cc1191a132a594
haleyjhankins <haley.j.hankins@gmail.com>
2017-04-21 15:54:21 -0500

    Got the curtains to work on hover

---
+++ day-29-something-useless/script.js
@@ -0,0 +1,8 @@
+var curtainWrapper = document.querySelector('.curtain_wrapper');
+var leftCurtain = document.querySelector('.curtainLeft');
+var rightCurtin = document.querySelector('.curtainRight');
+
+curtainWrapper.addEventListener('click', function (evt){
+  
+  console.log('Ahh you clicked me!');
+});
