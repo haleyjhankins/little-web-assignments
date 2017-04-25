33469c4ae2003774964489eb2c4378e7dcc4cf17
haleyjhankins <haley.j.hankins@gmail.com>
2017-04-24 21:49:09 -0500

    Got curtains and audio to work. Still need to add more gifs.

--- day-29-something-useless/script.js
+++ day-29-something-useless/script.js
@@ -1,8 +1,11 @@
-var curtainWrapper = document.querySelector('.curtain_wrapper');
+var curtainWrapper = document.querySelector('#curtainWrapper');
 var leftCurtain = document.querySelector('.curtainLeft');
-var rightCurtin = document.querySelector('.curtainRight');
+var rightCurtain = document.querySelector('.curtainRight');
+var homersVoice = new Audio('homervoice.mp4');
 
-curtainWrapper.addEventListener('click', function (evt){
-  
-  console.log('Ahh you clicked me!');
-});
+  curtainWrapper.onclick = function () {
+    homersVoice.play();
+    leftCurtain.style.marginLeft = "-600px";
+    rightCurtain.style.marginRight = "-600px";
+
+    };
